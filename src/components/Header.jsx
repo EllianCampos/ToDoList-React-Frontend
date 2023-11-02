08279ac8ca660eb936c334eb651db10f0216b02d
Ellian Campos <ellian.campos12@gmail.com>
2023-11-01 19:00:47 -0600

    cambio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,33 @@
 import Container from "react-bootstrap/Container";
 import { Button, Navbar, Nav } from "react-bootstrap";
 import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Header() {
   const [cookie, setCookie, removeCookie] = useCookies(["token"]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const logout = () => {
-    removeCookie("token")
-    navigate("/")
-  }
+    removeCookie("token");
+    navigate("/");
+  };
 
   return (
     <header>
-      <Navbar bg="success" data-bs-theme="dark"  variant="tabs" >
+      <Navbar bg="success" data-bs-theme="dark" variant="tabs">
         <Container>
           <Navbar.Brand href="/main">Gestor de tareas</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Item>
-              <Nav.Link href="/main">Mis Tareas</Nav.Link>
+              <Link to="/main" className="me-3">Mis Tareas</Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/config">Configuración</Nav.Link>
+              <Link to="/config"className="me-3">Configuración</Link>
             </Nav.Item>
-            <Nav.Item >
-              <Button variant="danger" onClick={logout}>Cerrar Sesión</Button>
+            <Nav.Item>
+              <Button variant="danger" onClick={logout}>
+                Cerrar Sesión
+              </Button>
             </Nav.Item>
           </Nav>
         </Container>
