61b313236b40fa54347c975925274d623aca7602
Ellian Campos <ellian.campos12@gmail.com>
2023-11-02 22:36:11 -0600

    removi el readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import Index from "./pages/Index"
 import Index from "./pages/"
 import Main from "./pages/Main.jsx"
 import Config from "./pages/Config.jsx"
