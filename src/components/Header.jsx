70a99cb7d50a1230ace828bf48c806343edbbc6e
Ellian Campos <ellian.campos12@gmail.com>
2023-11-02 22:45:21 -0600

    correccion del estilo del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,25 +13,49 @@ export default function Header() {
   };
 
   return (
+    // <header>
+    //   <Navbar bg="success" data-bs-theme="dark" variant="tabs">
+    //     <Container>
+    //       <Navbar.Brand href="/main">Gestor de tareas</Navbar.Brand>
+    //       <Nav className="me-auto">
+    //         <Nav.Item>
+    //           <Link to="/main" className="nav-link me-3">Mis Tareas</Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Link to="/config"className="nav-link me-3">Configuración</Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Button variant="danger" onClick={logout}>
+    //             Cerrar Sesión
+    //           </Button>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </Container>
+    //   </Navbar>
+    // </header>
+
     <header>
-      <Navbar bg="success" data-bs-theme="dark" variant="tabs">
-        <Container>
-          <Navbar.Brand href="/main">Gestor de tareas</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Item>
-              <Link to="/main" className="me-3">Mis Tareas</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/config"className="me-3">Configuración</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Button variant="danger" onClick={logout}>
-                Cerrar Sesión
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </Navbar>
-    </header>
+  <Navbar bg="success" expand="lg" data-bs-theme="dark" variant="tabs">
+    <Container>
+      <Navbar.Brand href="/main">Gestor de tareas</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarNav" />
+      <Navbar.Collapse id="navbarNav">
+        <Nav className="me-auto">
+          <Nav.Item>
+            <Link to="/main" className="nav-link text-white text-decoration-none">Mis Tareas</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/config" className="nav-link text-white text-decoration-none">Configuración</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Button variant="danger" onClick={logout}>
+              Cerrar Sesión
+            </Button>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+</header>
   );
 }
